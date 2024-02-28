2592910ebfd8289edd119cc80e5a3294d2c3385a
N-Rdiansyah <rdiansyah.muhammad@gmail.com>
2024-02-28 14:00:29 +0700

    Import Category

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+use Faker\Guesser\Name;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /** Seed the application's database.*/
+    public function run(): void
+    {
+        //Fake 10 data
+        \App\Models\User::factory(10)->create();
+
+        \App\Models\User::factory()->create([
+            'name' => 'Ahmad',
+            'email' => 'ahmad@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('12345678'), // password
+            'roles' => 'ADMIN',
+        ]);
+
+        //Tampilkan banyak seeder
+        $this->call([
+            CategorySeeder::class,
+            ProductSeeder::class
+
+        ]);
+    }
+}
---
+++ resources/views/components/auth-footer.blade.php
@@ -0,0 +1,3 @@
+  <div class="simple-footer">
+      Copyright &copy; Cafe - Resto Developer
+  </div>
